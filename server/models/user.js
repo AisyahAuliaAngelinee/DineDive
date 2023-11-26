16df956da83da4d2c5fee7132159e81790ed99c6
Aisyah Aulia Angelinee <vincentius.clarishna@gmail.com>
2023-11-26 17:33:33 +0700

    SERVER: setup database associations

---
+++ server/models/user.js
@@ -0,0 +1,50 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+	class User extends Model {
+		/**
+		 * Helper method for defining associations.
+		 * This method is not a part of Sequelize lifecycle.
+		 * The `models/index` file will call this method automatically.
+		 */
+		static associate(models) {
+			// define association here
+			this.hasMany(models.Cuisine, { foreignKey: "UserId" });
+		}
+	}
+	User.init(
+		{
+			username: {
+				type: DataTypes.STRING,
+				allowNull: false,
+				validate: {
+					notNull: true,
+					notEmpty: true,
+				},
+			},
+			email: {
+				type: DataTypes.STRING,
+				allowNull: false,
+				unique: true,
+				validate: {
+					isEmail: true,
+					notNull: true,
+					notEmpty: true,
+				},
+			},
+			password: {
+				type: DataTypes.STRING,
+				allowNull: false,
+				validate: {
+					notNull: true,
+					notEmpty: true,
+				},
+			},
+		},
+		{
+			sequelize,
+			modelName: "User",
+		}
+	);
+	return User;
+};
